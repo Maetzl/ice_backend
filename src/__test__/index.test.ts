573fd7d82750e125d7b00b03bee6a6352b7cb099
Matthias Wohland <wohland.matthias@gmail.com>
2023-05-26 02:02:43 +0200

    added tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,2 +1,15 @@
 import {describe, expect, test} from '@jest/globals';
-import {} from '../index';
+import {app} from '../index';
+import request from 'supertest';
+
+describe("Test the root path", () => {
+    test("It should response Backend", done => {
+      request(app)
+        .get("/")
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+          expect(response.text).toBe("Backend");
+          done();
+        });
+    });
+  });
