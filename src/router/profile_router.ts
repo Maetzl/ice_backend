46e34fb07a6f2c5b58b0c109d076701e5bf0f471
Matthias Wohland <32495722+Maetzl@users.noreply.github.com>
2023-06-16 02:45:39 +0200

    comment out console log

--- src/router/profile_router.ts
+++ src/router/profile_router.ts
@@ -10,7 +10,7 @@ const profileRouter = express.Router();
 profileRouter.post("/", validateAccessToken, (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: string): void; new(): any; }; }; }) => {
     const message = "Profile Data";
     
-    console.log(req.body);
+    //console.log(req.body);
     
     res.status(200).json(req.body);
   });
