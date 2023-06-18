9bbdb513740850d00f211834e44a992123d45f1c
Matthias Wohland <wohland.matthias@gmail.com>
2023-06-19 00:16:56 +0200

    added update Profile

--- src/router/profile_router.ts
+++ src/router/profile_router.ts
@@ -67,53 +67,34 @@ profileRouter.post("/", validateAccessToken, async (req : any, res :any) => {
 });
 
 profileRouter.post("/update", validateAccessToken, async (req : any, res :any) => {
-  //const userID = req.body.UserID;
-  //const mongoURI = process.env.MONGO_URL || "";
-  //  
-  //console.log(req.body.UserID);
-  //console.log("URL" + mongoURI);
-//
-  //try {
-  //  const client = await MongoClient.connect(mongoURI);
-  //  const db: Db = client.db("Ice");
-  //  const collection: Collection = db.collection('Users');
-//
-  //  const regexQuery = new RegExp(userID);
-  //  //const user = await collection.findOne({ playerID: { $regex: regexQuery } });
-//
-  //  const user = await collection.find({ playerID: userID }).toArray();
-//
-  //  if (user.length != 1) {
-  //    const result = await collection.insertOne({
-  //      "name": "",
-  //      "description": "",
-  //      "country": "",
-  //      "games": [
-  //      ],
-  //      "profilePictureUrl": "",
-  //      "playerID": userID
-  //    });
-//
-  //    const getInputedData = await collection.find({ playerID: userID }).toArray();
-//
-  //    res.status(200).json(getInputedData[0]);
-  //  }
-  //  else if(user.length == 1)
-  //  {
-  //    res.status(200).json(user[0]);
-  //  }
-  //  else
-  //  {
-  //    res.status(500).send('Multiple Documents with same ID');
-  //  }
-  //  
-  //  
-  //  client.close(); // Schließe die Verbindung zur Datenbank
-  //} catch (err) {
-  //  console.error('Fehler beim Ausführen der Abfrage:', err);
-  //  res.status(500).send('Interner Serverfehler');
-  //}
+  const userID = req.body.UserID;
+  const mongoURI = process.env.MONGO_URL || "";
   
+  try {
+    const client = await MongoClient.connect(mongoURI);
+    const db: Db = client.db("Ice");
+    const collection: Collection = db.collection('Users');
+
+    const user = await collection.updateOne({ playerID: userID }, {$set:{
+      name: req.body.Name,
+      description: req.body.Description,
+      country: req.body.Country
+    }});
+
+    if(user.modifiedCount == 1)
+    {      
+      res.status(200).json();
+    }
+    else
+    {
+      res.status(500).send('Multiple Documents with same ID');
+    }
+
+    client.close(); // Schließe die Verbindung zur Datenbank
+  } catch (err) {
+    console.error('Fehler beim Ausführen der Abfrage:', err);
+    res.status(500).send('Interner Serverfehler');
+  }
 });
 
 export {profileRouter}
